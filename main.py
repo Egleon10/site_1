cf0dfcdbc3ca667a4b797303906237bdbefc8346
Egleon10 <orel.eagle.nik@gmail.com>
2024-01-21 01:09:01 +0300

    base

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print('alekjrhnfv')
-print('ljhfv')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello world"
+
+
+@app.route('/about')
+def about():
+    return "About page"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
